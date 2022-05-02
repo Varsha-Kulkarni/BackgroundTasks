7243fa304f1e197c6e5803bb8f9e6e5dc5068cf0
Varsha Kulkarni <varsha@triveous.com>
2022-05-02 15:51:17 +0530

    Create common constants

---
+++ common/src/main/java/com/varshakulkarni/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.varshakulkarni.common
+
+class Constants {
+    object Notifications {
+        const val CHANNEL_ID = "Schedulers"
+        const val CHANNEL_NAME = "Schedulers"
+        const val NOTIFICATION_ID = 111
+    }
+}
